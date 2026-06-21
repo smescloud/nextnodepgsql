13c394c7742574c8ee6fdff6aa3878fed6da3fbf
smescloud <smescloud@gmail.com>
2026-06-21 15:29:08 +0300

    Changed index.js

--- backend/index.js
+++ backend/index.js
@@ -239,8 +239,8 @@ app.delete('/users/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Backend API listening on port ${PORT}`);
 });
 
 async function disconnectPrisma(signal) {
